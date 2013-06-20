f86a4b136afe1c1bfb35e8229afcced5cee9e97e
Stamatis <stam.mavrikis@gmail.com>
2013-06-20 06:09:45 +0300

    making ajax work
    03 making ajax work
    03 edit weights

--- 031instsql.php
+++ 031instsql.php
@@ -4,15 +4,17 @@ include_once ('evform.php');
 
 $inst_id=$_GET['inst_id'];
 $new_instruction_text=$_GET['insturction_text'];
-echo $inst_id;
-////update an instruction with the new text
-//$update_instruction='UPDATE instructions SET 
-//    instructions='.$new_instruction_text.'WHERE instructions_id='.$inst_id;
-//
-//// select instruction with a specific id
+
+
+//update an instruction with the new text
+$update_instruction="UPDATE instructions SET 
+    instructions='".$new_instruction_text."' WHERE instructions_id=".$inst_id;
+mysqli_query($con,$update_instruction);
+echo  ' Intsruction '.$inst_id.' was saved';
+// select instruction with a specific id
 //$select_instruction="SELECT instructions FROM 
 //    instructions WHERE instructions_id=".$inst_id;
 //$select_instruction_fetch=mysqli_query($con,$select_instruction);
 
-
+mysqli_close($con);
 ?>
--- 03questions.php
+++ 03questions.php
@@ -10,7 +10,7 @@
 
  
  if (isset($_GET['category_id'])){
-      $question_query= "SELECT * FROM question WHERE cid=".$_GET['category_id'];
+      $question_query= "SELECT * FROM question WHERE cid=".$_GET['category_id']." GROUP BY number";
       $question_fetch=mysqli_query($con,$question_query);
     //boolean  for questions or instructions
        $is_question=true;
@@ -20,6 +20,7 @@
      $instruction_fetch=mysqli_query($con,$instruction_query);
       $is_question=false;
       
+      
       //  $query1= "SELECT * FROM qvalues WHERE question_id=1";
 //  $fetch1=mysqli_query($con,$query1);
 //  while($result1 = mysqli_fetch_array($fetch1)) {
@@ -27,6 +28,16 @@
 //    value='".$result1["qv_value"]."'>
 //    <button class='btn'>Delete</button>";}
  }
+ 
+ // Select all the names of the attritubes
+ $attr_names='SELECT * FROM attribute';
+ $attr_names_fetch=mysqli_query($con,$attr_names);
+ 
+ // Finding the number of the current attributes (important for the attribute weights columns)
+ $attr_names_no='SELECT COUNT(*) FROM attribute';
+ $attr_names_no_fetch=mysqli_query($con,$attr_names_no);
+ $attr_names_no_result=mysqli_fetch_array( $attr_names_no_fetch);
+
 ?>
 <style>
   .table th {text-align:center; vertical-align:middle;}
@@ -45,7 +56,7 @@
 
     </head>
     <body>
-       <div id='showme'>The result here</div>
+       
           <!--select category div-->
         <div style=" width:200px;float:left;">
             
@@ -66,45 +77,75 @@
         <div style="float:left; border-left: 1px solid;  width:1000px">
         <table class="table table-hover">
             <?php 
+            //if it is a categoty with questions
             if ($is_question==true){
           echo  '<tr><th>No</th><th>Text</th><th>Question Weight</th><th>Connection</th></th><th>Delete</th></tr>';
        
-   $no=1;
+  
  while($result = mysqli_fetch_array($question_fetch)) {
-    echo "<tr><td>".$no."</td><td><div class='accordion-heading'>";
+    echo "<tr><td>".$result["number"]."</td><td><div class='accordion-heading'>";
     echo "<a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion2' href='#collapse".$result['question_id']."'>";
     echo $result["qtext"]."</a></div></td><td>" .$result["qweight"]."</td> 
         <td>coming soon </td> <td><button class='btn'>Delete</button> </td></div></tr>";
      echo "<tr><td colspan='5' style='  border-top-width: 0px; padding-top:0px; padding-bottom: 0px;'>
          <div id='collapse".$result['question_id']."' class='accordion-body collapse'>";
      
-                    //creating the edit questions table
-                echo    "<div class='accordion-inner'><h5 class='nav-header'> edit Question </h5>";
-              echo "<table style='text-align:center;'><tr><th rowspan='2' class='text-justify'>No</th><th rowspan='2' class='text-justify'>Question Text</th><th rowspan='2'>Question Weight</th><th colspan='3'>Attribute Weights</th></tr>
-                  <tr><th>Manager</th><th>Programmer</th><th>Accountant</th></tr>
-                    <tr> <td><input type='text' class='input-small'></td><td><input type='text'></td> <td><input type='text'class='input-small'></td><td><input type='text' class='input-small'></td><td><input type='text' class='input-small'></td>
-                    <td><input type='text' class='input-small'></td></tr></table><br>";
-                     echo '<p>no Connection</p>';
+                    //creating the edit questions accordion (edit weights, edit question, edit answers, edit connection)
+     
+            //edit weights
+                echo    "<div class='accordion-inner'><h5 class='nav-header'> edit Weights </h5>";
+              echo "<table style='text-align:center;'><tr><th rowspan='2'>Question Weight</th>
+                  <th colspan='".$attr_names_no_result['COUNT(*)']."'>Relative Attribute Weights</th><th>Save</th></tr>";
+    echo  '<tr>';          
+while($attr_names_result = mysqli_fetch_array( $attr_names_fetch)){
+ echo '<th>'.$attr_names_result["attr_title"].'</th>';
+ }
+ $attr_names_fetch=mysqli_query($con,$attr_names); //retrieving again the attributes 
+   echo "</tr><tr><td><input type='text'class='input-small' value=".$result["qweight"]."></td>";
+   while($attr_names_result = mysqli_fetch_array( $attr_names_fetch)){
+       // select weight according to attribute and question
+       $attr_question_weight="SELECT ev_value FROM evweight WHERE attr_id ='".$attr_names_result["attr_id"].
+               "' AND question_id ='".$result['question_id']."'";
+       $attr_question_weight_fetch=mysqli_query($con,$attr_question_weight);
+        $attr_question_weight_result= mysqli_fetch_array($attr_question_weight_fetch);
+   echo   "<td><input type='text'class='input-small' value=".$attr_question_weight_result['ev_value']."></td>"; 
+   }
+   echo "<td><button class='btn'>Save</button></td></tr></table>";
+            $attr_names_fetch=mysqli_query($con,$attr_names); //retrieving again the attributes         
+                     
+                     //edit question text and number - questions are more sensitie data
+                     echo    "<h5 class='nav-header'> edit Questions </h5>";
+              echo "<table style='text-align:center;'><tr><th class='text-justify'>No</th>
+                  <th rclass='text-justify'>Question Text</th><th>Hide</th><th>Invalid</th><th>Save</th><tr>
+                  <tr> <td><input type='text' class='input-small'></td>
+                  <td><input type='text' style='width:600px'></td>
+                  <td><input type='checkbox' ></td><td><input type='checkbox' ></td>
+                  <td><button class='btn'>Save</button></td></tr></table>";
+              
                   // creating the edit possible answers table 
-                  echo "<h5 class='nav-header'> edit answers for this question </h5><br>
+                  echo "<h5 class='nav-header'> edit answers for this question </h5>
                       <table><tr><th>Text</th><th>Value</th></tr>";
                    for( $i=1; $i<11; $i++){  
-                      echo "<tr><td><input type='text'></td><td><input type='text' class='input-small' value='$i'></td></tr>"; 
+                      echo "<tr><td><input type='text' style='width:600px'></td>
+                          <td><input type='text' class='input-small' value='$i'></td></tr>"; 
                    }
                  echo '</table>';  
                  
+                 // buttons for saving Answers and edit Connections
                echo    "<a href='#editConnection' role='button' class='btn' data-toggle='modal'>Manage Connections</a><button class='btn'>Save</button><button class='btn'>Cancel</button>";
 
-echo '</div></div></td></tr>';
-$no++;
+echo '</div></div>';
+
 }
-            }
+            } //end of questions table
+            
+            
    else{   // creating instructions table
         $no=1;
  echo  '<tr><th>No</th><th>Text</th></th><th>Save</th><th>Delete</th></tr>';
  while($result = mysqli_fetch_array($instruction_fetch)) {
      echo '<form name="instruction_form"';
-echo "<tr><td>".$no."</td><td><div id='instruction_id".$result["instructions_id"]."'>fsdgdgdf".
+echo "<tr><td>".$no."</td><td><div id='instruction_id".$result["instructions_id"]."' style='display:none;'>".
         $result["instructions_id"]."</div>
     <textarea rows='6' id='instarea".$result["instructions_id"].
         "'style='width:600px' >".$result["instructions"]."</textarea></td> <td>
@@ -202,7 +243,7 @@ $no++;
          <button class='btn' data-dismiss="modal" aria-hidden="true">Cancel</button>
           </div>
   </div>    
- 
+  <div id='showme' style='position:fixed; bottom:0px; color:#3333CC;'></div>
      <script language="javascript" type="text/javascript">
 //using AJAX for instructions
 //Browser Support Code for AJAX
@@ -238,14 +279,72 @@ function ajaxFunction(instarea,instid){
  // Now get the value from user and pass it to
  // server script.
  var insturction_text = document.getElementById(instarea).value;
-var ajax_instruction_id = document.getElementById(instid).value;
+var ajax_instruction_id = document.getElementById(instid).innerHTML;
+
  var queryString = "?insturction_text=" + insturction_text ;
  queryString +=  "&inst_id=" + ajax_instruction_id;
  ajaxRequest.open("GET", "031instsql.php" + 
                               queryString, true);
  ajaxRequest.send(null); 
+ 
+ //fading in fade out showme informing the admin for the saving progress
+ $("#showme").fadeIn(3000);
+ $("#showme").fadeOut(3000);
+ 
 }
-</script>  
+
+//using Ajax to save weights
+function ajaxWeight(question_id,question_weight,attr_weights){
+ var ajaxRequest;  // The variable that makes Ajax possible!
+	
+ try{
+   // Opera 8.0+, Firefox, Safari
+   ajaxRequest = new XMLHttpRequest();
+ }catch (e){
+   // Internet Explorer Browsers
+   try{
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+   }catch (e) {
+      try{
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }catch (e){
+         // Something went wrong
+         alert("Your browser broke!");
+         return false;
+      }
+   }
+ }
+ // Create a function that will receive data 
+ // sent from the server and will update
+ // div section in the same page.
+ ajaxRequest.onreadystatechange = function(){
+   if(ajaxRequest.readyState == 4){
+      var ajaxDisplay = document.getElementById('showme');
+      ajaxDisplay.innerHTML = ajaxRequest.responseText;
+   }
+ }
+ // Now get the value from user and pass it to
+ // server script.
+ var insturction_text = document.getElementById(instarea).value;
+var ajax_instruction_id = document.getElementById(instid).innerHTML;
+
+ var queryString = "?insturction_text=" + insturction_text ;
+ queryString +=  "&inst_id=" + ajax_instruction_id;
+ ajaxRequest.open("GET", "031instsql.php" + 
+                              queryString, true);
+ ajaxRequest.send(null); 
+ 
+ //fading in fade out showme informing the admin for the saving progress
+ $("#showme").fadeIn(3000);
+ $("#showme").fadeOut(3000);
+ 
+}
+
+</script> 
+<?php
+// always closing connection
+mysqli_close($con);
+?>
     </body>
     
 </html>
